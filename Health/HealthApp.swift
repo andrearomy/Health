34b7b22c4b39b9a112964ce1c9b6fb7bb5078274
andrearomy <andry21yt@gmail.com>
2023-11-13 19:16:28 +0100

    Initial Commit

---
+++ Health/HealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthApp.swift
+//  Health
+//
+//  Created by Andrea Romano on 13/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
